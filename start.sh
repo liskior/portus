6b5016acceddebe5b51789df23886dc7177cda20
liskior <liskior@gmail.com>
2017-11-23 02:46:29 +0100

    var teco

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 mkdir -p {certs,data}
 source .env
+find . -type f -exec sed -i 's/${PORTUS_MACHINE_FQDN_VALUE}/${PORTUS_MACHINE_FQDN_VALUE}/g' {} \;
 sudo certbot certonly --agree-tos --standalone -d $PORTUS_MACHINE_FQDN_VALUE --email $EMAIL
 cp /etc/letsencrypt/live/$PORTUS_MACHINE_FQDN_VALUE/* certs/
