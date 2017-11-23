ece63bf03d368028799c9172f51589d508a29575
liskior <liskior@gmail.com>
2017-11-23 02:36:21 +0100

    replace fqdn auto

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 mkdir -p {certs,data}
-cd portus
+find . -type f -exec sed -i 's/${PORTUS_MACHINE_FQDN_VALUE}/${PORTUS_MACHINE_FQDN_VALUE}/g' {} \;
 sudo certbot certonly --agree-tos --standalone -d portus.liskior.me --email admin@portus.liskior.me
 cp /etc/letsencrypt/live/portus.liskior.me/* certs/
-
