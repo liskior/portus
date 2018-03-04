45b033ad24582ecc7058fa93aa1e9827ce9aa46f
Anastasia  Kryzhanovskaya <liskior@gmail.com>
2018-03-04 20:50:14 +0100

    new version

--- start.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p {certs,data}
-source .env
-str='s/portus.staging.teco.edu/'$PORTUS_MACHINE_FQDN_VALUE'/g'
-find ./* -type f -exec sed -i $str {} \;
-sudo certbot certonly --agree-tos --standalone -d $PORTUS_MACHINE_FQDN_VALUE --email $EMAIL
-cp /etc/letsencrypt/live/$PORTUS_MACHINE_FQDN_VALUE/* certs/
